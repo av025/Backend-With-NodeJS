5e5e55d93df9c78bcdfc86e11787935306fbbef1
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-21 21:47:49 +0530

    Introduction to Loops

---
+++ 01.Javascript Refresher/08. Introduction to loops in javascript/script.js
@@ -0,0 +1,11 @@
+//! Introduction to Loops in Javascript 
+// We used Loops to do repeatative task for certain duration or time and follow DRY Principle which stands for "Don't Repeat Yourself"
+
+//* In JS Language we have different types of loop 
+// 1st. while loop 
+// 2nd. for loop  
+//   -- type of for loop  
+//     a. for of loop 
+//     b. for in loop 
+//     c. forEach() loop 
+// 3rd. do while loop 
